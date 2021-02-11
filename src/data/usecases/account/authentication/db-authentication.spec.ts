95d47916d3aa1e0e88aa373b42fd1547e2cf367c
Wisinewski <g_gomees@hotmail.com>
2021-02-11 19:12:12 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/account/authentication/db-authentication.spec.ts
+++ src/data/usecases/account/authentication/db-authentication.spec.ts
@@ -1,23 +1,41 @@
+import { LoadAccountByEmailRepositorySpy } from './../../../test/mock-load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
 import { AccountModel } from '../../../../domain/models/account'
 import { LoadAccountByEmailRepository } from './../../../protocols/db/account/load-account-by-email-repository'
 
-describe('Name of the group', () => {
-  test('should call LoadAccountByEmailRepository with correct values', async () => {
-    class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
-      email
-      async loadAccountByEmail (email: string): Promise<AccountModel> {
-        this.email = email
-        return {
-          id: 'any_id',
-          name: 'any_name',
-          email: 'any_email',
-          password: 'any_password'
-        }
+const mockLoadAccountByEmailRepository = (): LoadAccountByEmailRepositorySpy => {
+  class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
+    email
+    async loadAccountByEmail (email: string): Promise<AccountModel> {
+      this.email = email
+      return {
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password'
       }
     }
-    const loadAccountByEmailRepositorySpy = new LoadAccountByEmailRepositorySpy()
-    const sut = new DbAuthentication(loadAccountByEmailRepositorySpy)
+  }
+  return new LoadAccountByEmailRepositorySpy()
+}
+
+type SutTypes = {
+  sut: DbAuthentication
+  loadAccountByEmailRepositorySpy: LoadAccountByEmailRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositorySpy = mockLoadAccountByEmailRepository()
+  const sut = new DbAuthentication(loadAccountByEmailRepositorySpy)
+  return {
+    sut,
+    loadAccountByEmailRepositorySpy
+  }
+}
+
+describe('Name of the group', () => {
+  test('should call LoadAccountByEmailRepository with correct values', async () => {
+    const { sut, loadAccountByEmailRepositorySpy } = makeSut()
     await sut.auth({
       email: 'any_email',
       password: 'any_password'
