1b8adfcfee00af26eebe7d581ae219c3f828c578
Wisinewski <g_gomees@hotmail.com>
2021-02-13 19:17:02 -0300

    feat: ensure DbAuthentication calls Encrypter with correct id

--- src/data/test/mock-cryptography.ts
+++ src/data/test/mock-cryptography.ts
@@ -1,12 +1,22 @@
+import { Encrypter } from './../protocols/criptography/encrypter'
 import { HashComparer } from '../protocols/criptography/hash-comparer'
 
 export class HashComparerSpy implements HashComparer {
-  plainText: string
+  plaintext: string
   digest: string
   result: boolean = true
-  async compare (plainText: string, digest: string): Promise<boolean> {
-    this.plainText = plainText
+  async compare (plaintext: string, digest: string): Promise<boolean> {
+    this.plaintext = plaintext
     this.digest = digest
+    return this.result
+  }
+}
+
+export class EncrypterSpy implements Encrypter {
+  plaintext: string
+  result: string = 'encrypted_plaintext'
+  encrypt (plaintext: string): string {
+    this.plaintext = plaintext
     return this.result
   }
 }
--- src/data/usecases/account/authentication/db-authentication.ts
+++ src/data/usecases/account/authentication/db-authentication.ts
@@ -1,3 +1,4 @@
+import { Encrypter } from './../../../protocols/criptography/encrypter'
 import { LoadAccountByEmailRepository } from './../../../protocols/db/account/load-account-by-email-repository'
 import { Authentication, AuthenticationParams } from './../../../../domain/usecases/account/authentication'
 import { HashComparer } from '../../../protocols/criptography/hash-comparer'
@@ -5,14 +6,18 @@ import { HashComparer } from '../../../protocols/criptography/hash-comparer'
 export class DbAuthentication implements Authentication {
   constructor (
     private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-    private readonly hashComparer: HashComparer
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
   ) {}
 
   async auth (params: AuthenticationParams): Promise<string> {
     const { email, password } = params
     const account = await this.loadAccountByEmailRepository.loadAccountByEmail(email)
     if (account) {
-      await this.hashComparer.compare(password, account.password)
+      const isValid = await this.hashComparer.compare(password, account.password)
+      if (isValid) {
+        await this.encrypter.encrypt(account.id)
+      }
     }
     return null
   }
