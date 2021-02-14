18fc012a361dcfe3722692d7bdaf7eb072a4df3c
Wisinewski <g_gomees@hotmail.com>
2021-02-14 17:46:59 -0300

    refactor: change mock LoadAccountByEmailRepository return from string to AccountModel

--- src/data/test/mock-db-account.ts
+++ src/data/test/mock-db-account.ts
@@ -1,14 +1,14 @@
+import { AccountModel } from './../../domain/models/account'
 import { UpdateAccessTokenRepository } from './../protocols/db/account/update-access-token-repository'
-import { AccountModel } from '../../domain/models/account'
 import { mockAccountModel } from '../../domain/test/mock-account'
 import { LoadAccountByEmailRepository } from '../protocols/db/account/load-account-by-email-repository'
 
 export class LoadAccountByEmailRepositorySpy implements LoadAccountByEmailRepository {
   email: string
-  result: string
+  result: AccountModel = mockAccountModel()
   async loadAccountByEmail (email: string): Promise<AccountModel> {
     this.email = email
-    return mockAccountModel()
+    return this.result
   }
 }
 
