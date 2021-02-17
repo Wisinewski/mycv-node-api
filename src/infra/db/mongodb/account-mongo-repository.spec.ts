655f414548058bdc733071eb94b73d4064387c8c
Wisinewski <g_gomees@hotmail.com>
2021-02-17 13:44:01 -0300

    test: ensure LoadAccountByEmailRepository returns an account on success

---
+++ src/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -0,0 +1,37 @@
+import { AccountMongoRepository } from './account-mongo-repository'
+import { mockAddAccountParams } from './../../../domain/test/mock-account'
+import { Collection } from 'mongodb'
+import { MongoHelper } from './mongo-helper'
+import env from '../../../main/config/env'
+
+const makeSut = (): AccountMongoRepository => {
+  const sut = new AccountMongoRepository()
+  return sut
+}
+
+let accountCollection: Collection
+
+describe('AccountMongoRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('loadByEmail()', () => {
+    test('should return an account on success', async () => {
+      const sut = makeSut()
+      const addAccountParams = mockAddAccountParams()
+      await accountCollection.insertOne(addAccountParams)
+      const account = await sut.loadAccountByEmail(addAccountParams.email)
+      expect(account).toBeTruthy()
+    })
+  })
+})
