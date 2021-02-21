e1890f78f4c809fb0349cac4affd45895645673c
Wisinewski <g_gomees@hotmail.com>
2021-02-21 20:37:18 -0300

    test: ensure BcryptAdapter returns true if compare succeeds

--- src/infra/db/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/db/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import { BcryptAdapter } from './bcrypt-adapter'
 import bcrypt from 'bcrypt'
 
+jest.mock('bcrypt', () => ({
+  async compare (): Promise<boolean> {
+    return true
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   describe('compare()', () => {
     test('should call compare with correct values', async () => {
@@ -8,6 +14,12 @@ describe('Bcrypt Adapter', () => {
       const compareSpy = jest.spyOn(bcrypt, 'compare')
       await sut.compare('any_value', 'any_hash')
       expect(compareSpy).toHaveBeenCalledWith('any_value', 'any_hash')
+    })
+
+    test('should return true if compare succeeds', async () => {
+      const sut = new BcryptAdapter()
+      const isValid = await sut.compare('any_value', 'any_hash')
+      expect(isValid).toBe(true)
     })
   })
 })
