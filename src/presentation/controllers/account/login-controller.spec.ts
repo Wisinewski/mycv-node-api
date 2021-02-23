5ca103883b5d68b3d7cfd0947c1e175e79f80983
Wisinewski <g_gomees@hotmail.com>
2021-02-22 21:45:17 -0300

    test: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/account/login-controller.spec.ts
+++ src/presentation/controllers/account/login-controller.spec.ts
@@ -1,5 +1,26 @@
-describe('Login Controller', () => {
-  test('should call Authentication with correct values ', () => {
+import { LoginController } from './login-controller'
+import { AuthenticationSpy } from './../../test/account/mock-account'
+import { mockAuthenticationParams } from './../../../domain/test/mock-account'
 
+type SutTypes = {
+  authenticationSpy: AuthenticationSpy
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationSpy = new AuthenticationSpy()
+  const sut = new LoginController(authenticationSpy)
+  return {
+    sut,
+    authenticationSpy
+  }
+}
+
+describe('Login Controller', () => {
+  test('should call Authentication with correct values ', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    const request = mockAuthenticationParams()
+    await sut.handle(request)
+    expect(authenticationSpy.params).toEqual(request)
   })
 })
---
+++ src/presentation/test/account/mock-account.ts
@@ -0,0 +1,11 @@
+import { Authentication, AuthenticationParams } from './../../../domain/usecases/account/authentication'
+
+export class AuthenticationSpy implements Authentication {
+  params: AuthenticationParams
+  result: string = 'any_accessToken'
+
+  async auth (params: AuthenticationParams): Promise<string> {
+    this.params = params
+    return this.result
+  }
+}
