0e44a3a6025822bc8fe17b1a0eb4cf3bcc5b3c72
Wisinewski <g_gomees@hotmail.com>
2021-03-02 19:31:19 -0300

    refactor: change AuthenticationSpy result value

--- src/presentation/test/account/mock-account.ts
+++ src/presentation/test/account/mock-account.ts
@@ -2,7 +2,7 @@ import { Authentication, AuthenticationParams } from './../../../domain/usecases
 
 export class AuthenticationSpy implements Authentication {
   params: AuthenticationParams
-  result: string = 'any_accessToken'
+  result: string = 'any_token'
 
   async auth (params: AuthenticationParams): Promise<string> {
     this.params = params
