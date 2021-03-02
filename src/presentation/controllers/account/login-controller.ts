713bdec44395fb16ff7c833ccc9fdb24c63e8abd
Wisinewski <g_gomees@hotmail.com>
2021-03-02 19:32:10 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/account/login-controller.ts
+++ src/presentation/controllers/account/login-controller.ts
@@ -1,5 +1,4 @@
-import { unauthorized } from './../../helpers/http-helper'
-import { serverError } from '../../helpers/http-helper'
+import { unauthorized, ok, serverError } from './../../helpers/http-helper'
 import { Authentication } from './../../../domain/usecases/account/authentication'
 import { Controller } from './../../protocols/controller'
 import { HttpResponse } from './../../protocols/http'
@@ -15,6 +14,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
